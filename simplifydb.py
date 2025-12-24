fb4b6f1d079f73307c9f78bc0f94ecdd9e12a2f6
Hanno Böck <990588+hannob@users.noreply.github.com>
2025-12-24 09:41:32 +0100

    more renames and different order of columns for simplified prtr table

--- simplifydb.py
+++ simplifydb.py
@@ -8,6 +8,7 @@ con = MySQLdb.connect(cursorclass=MySQLdb.cursors.DictCursor, host="127.0.0.1",
                       database="eu", user="ghg")
 c = con.cursor()
 
+c.execute("DROP TABLE IF EXISTS prtr")
 c.execute("CREATE TABLE prtr LIKE prtrraw")
 c.execute("INSERT INTO prtr SELECT * FROM prtrraw")
 
@@ -16,10 +17,25 @@ c.execute("ALTER TABLE prtr DROP COLUMN `TargetRelease`")
 
 c.execute("ALTER TABLE prtr RENAME COLUMN `countryName` TO `country`")
 c.execute("ALTER TABLE prtr RENAME COLUMN `reportingYear` TO `year`")
-c.execute("ALTER TABLE prtr RENAME COLUMN `facilityName` TO `name`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `EPRTR_SectorCode` TO `sectorcode`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `EPRTR_SectorName` TO `sectorname`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `EPRTRAnnexIMainActivity` TO `annexiactivity`")
 c.execute("ALTER TABLE prtr RENAME COLUMN `facilityInspireId` TO `inspireid`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `facilityName` TO `name`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `Longitude` TO `lon`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `Latitude` TO `lat`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `addressConfidentialityReason` TO `addrconfreason`")
 c.execute("ALTER TABLE prtr RENAME COLUMN `Pollutant` TO `pollutant`")
 c.execute("ALTER TABLE prtr RENAME COLUMN `Releases` TO `releases`")
+c.execute("ALTER TABLE prtr RENAME COLUMN `ConfidentialityReason` TO `confreason`")
+
+c.execute("ALTER TABLE prtr MODIFY `addrconfreason` VARCHAR(32) CHARACTER SET ascii NOT NULL AFTER `releases`")
+c.execute("ALTER TABLE prtr MODIFY `annexiactivity` VARCHAR(16) CHARACTER SET ascii NOT NULL AFTER `releases`")
+c.execute("ALTER TABLE prtr MODIFY `sectorname` VARCHAR(128) CHARACTER SET ascii NOT NULL AFTER `releases`")
+c.execute("ALTER TABLE prtr MODIFY `sectorcode` TINYINT unsigned NOT NULL AFTER `releases`")
+c.execute("ALTER TABLE prtr MODIFY `lat` VARCHAR(32) CHARACTER SET ascii NOT NULL AFTER `releases`")
+c.execute("ALTER TABLE prtr MODIFY `lon` VARCHAR(32) CHARACTER SET ascii NOT NULL AFTER `releases`")
+c.execute("ALTER TABLE prtr MODIFY `city` VARCHAR(128) NOT NULL AFTER `releases`")
 
 c.execute("SELECT pollutant FROM prtr GROUP BY pollutant")
 for p in c.fetchall():
