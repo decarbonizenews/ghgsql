227334d64baf7a0681857e9f920eed792fb962d4
Hanno Böck <990588+hannob@users.noreply.github.com>
2026-01-13 17:38:12 +0100

    Don't use Apache foreground process, avoid unexpected `SIGWINCH` shutdowns
    The `SIGWINCH` signal gets sent when a terminal is resized. However, Apache
    re-uses this as a "shutdown" signal. (While an obscure choice, this is
    intentional by Apache.)
    This has the unintended effect that resizing a terminal window shuts
    down any container using apache as its main process.
    
    Solving this by starting Apache in the background and using a simple
    `sleep inf` as the main container process.
    
    See also: https://github.com/docker-library/httpd/issues/280

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@
 mysqld --user=root &
 
 echo "\$cfg['blowfish_secret'] ='$(head -c 20 /dev/urandom | base64)';" >>/var/www/html/config.inc.php
-apachectl -D FOREGROUND
+
+apachectl start
+
+sleep inf
